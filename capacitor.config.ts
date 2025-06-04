03926be7548f0b0db54d8c535954e4b4312aefb5
ashish <ashish@akeo.no>
2025-06-04 17:07:17 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agent-app',
+  webDir: 'dist'
+};
+
+export default config;
